a020740f1ea383d2f64d16df0bb7a67c80f03797
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-09 22:23:11 -0500

    modification cours16

--- Evaluations/TP2/Views/about.php
+++
@@ -1,7 +0,0 @@
-{{ include('layouts/header.php', {title:'Error 404'})}}
-    <div class="container">
-        <h2>Error</h2>
-        <strong class="error"> 404 page not found! </strong>
-        <p>{{ msg }}</p>
-    </div>
-{{ include('layouts/footer.php')}}
--- Evaluations/TP2/Views/layout/footer.php
+++
@@ -1,7 +0,0 @@
-
-    </main>
-        <footer>
-            <p>2024 All rights reserved</p>
-        </footer>
-    </body>
-</html>
