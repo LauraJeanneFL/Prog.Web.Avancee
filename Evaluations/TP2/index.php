e9fbfa75f72039b198a0badbaf499c559a976d6d
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-13 09:49:24 -0500

    Ajout des fichiers racine

---
+++ Evaluations/TP2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Activer l'affichage des erreurs pour le débogage
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'config.php';
+require_once 'vendor/autoload.php';
+require_once 'Routes/web.php';
